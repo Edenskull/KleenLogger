e1b0d2389d0cee3df8785e53b6f719cd0b2135ea
Edenskull <maxchamp@live.fr>
2019-02-19 17:00:08 +0100

    Refactor Logger

--- pylogger.py
+++ pylogger.py
@@ -3,11 +3,14 @@ from time import strftime
 
 filename = "log/{}-{}.log".format("myapp", strftime("%Y_%m_%d_%H_%M_%S"))
 
-logging.basicConfig(
-    level=logging.INFO,
-    filename=filename,
-    format="[%(asctime)s][%(levelname)s] : [%(filename)s][%(funcName)s] : %(message)s",
+pylogger = logging.getLogger(__name__)
+pylogger.setLevel(logging.DEBUG)
+
+handler = logging.FileHandler(filename, 'w', 'utf-8')
+formatter = logging.Formatter(
+    fmt='[%(asctime)s][%(levelname)s] : [%(filename)s][%(funcName)s] : %(message)s',
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
-pylogger = logging.getLogger()
+handler.setFormatter(formatter)
+pylogger.addHandler(handler)
