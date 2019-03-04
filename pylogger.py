b75e2a98788dbeaa1d43d537a7245db03be3685b
Edenskull <maxchamp@live.fr>
2019-03-04 23:06:40 +0100

    Globalize Module

--- pylogger.py
+++ pylogger.py
@@ -1,11 +1,22 @@
 import logging
+import os
+import configparser
 
 from time import strftime
 
-filename = "log/{}-{}.log".format("myapp", strftime("%Y_%m_%d_%H_%M_%S"))
+
+if not os.path.exists('log'):  # Create the folder if it doesn't exists
+    os.mkdir('log')
+
+root = os.path.dirname(os.path.realpath(__file__))
+
+config = configparser.ConfigParser()
+config.read(os.path.join(root, 'logger.ini'))
+
+filename = "log/{}-{}.log".format(config['Appname'], strftime("%Y_%m_%d_%H_%M_%S"))
 
 pylogger = logging.getLogger(__name__)
-pylogger.setLevel(logging.DEBUG)
+pylogger.setLevel(logging.getLevelName(config['Level']))
 
 handler = logging.FileHandler(filename, 'w', 'utf-8')
 formatter = logging.Formatter(
